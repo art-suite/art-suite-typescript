77678c068de12d9140440a6db96e822014619177
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2023-11-06 11:56:57 +0100

    patch/doc: removed build files

--- packages/art-ery-react-js/build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('./ArtSuite/ArtEryReactJs');});
-//# sourceMappingURL=index.js.map
--- packages/art-ery-react-js/build/test/ArtEryReactJs.test.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return Caf.importInvoke(["test", "initArtEry", "createJavaScriptPipeline", "assert", "Object"], [global, require('./StandardImport'), require('./JavaScriptPipelineLib')], (test, initArtEry, createJavaScriptPipeline, assert, Object) => {return test("initArtEry with one class", function() {let models, pipelines, pipelineRegistry, subscriptions; ({models, pipelines, pipelineRegistry, subscriptions} = initArtEry([createJavaScriptPipeline()])); assert.eq(Object.keys(pipelines), ["user"]); assert.eq(Object.keys(models), ["user"]); return assert.eq(Object.keys(subscriptions), ["useUser"]);});});});
-//# sourceMappingURL=ArtEryReactJs.test.js.map
